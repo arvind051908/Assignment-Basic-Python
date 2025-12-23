8bf879c21a51cf69e7405eac87653ff6a18bed94
Arvind Kumar <arvindk88641@gmail.com>
2025-12-23 21:17:25 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+# Takeing input Two Number From The User
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Adding Two Number
+c = a + b
+print("Addition =", c)
+
+# Subtracting Two Number
+c = a - b
+print("Subtraction =", c)
+
+# Multiplying Two Number
+c = a * b
+print("Multiplication =", c)
+
+# Dividing Two Number
+c = a / b
+print("Division =", c)
---
+++ task2.py
@@ -0,0 +1,6 @@
+# Task 2: Create a Personalized Greeting
+# Take input from the user first_name and last_name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello",",",full_name,"!","Welcome to The Python Programm")
